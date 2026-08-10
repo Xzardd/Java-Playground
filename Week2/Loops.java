940e127e49dfc11e5b8d4b7085a2b65d407ce131
Farhan Raza <farhanwork08@gmail.com>
2026-08-11 00:52:40 +0530

    Fixed Loops file

--- Week2/Loops.java
+++ Week2/Loops.java
@@ -1,11 +1,10 @@
 public class Loops {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-
-        int x= 0;
+   public static void main(String[] args) {
+    System.out.println("fixed it");
+   }
 
     }
     //hi boy
     
-}
+
