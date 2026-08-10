9c75e6474b79ce022733b4c96f83cbf6df20242b
Farhan Raza <farhanwork08@gmail.com>
2026-08-11 00:59:46 +0530

    Update Loops.java

--- Week2/Loops.java
+++ Week2/Loops.java
@@ -1,10 +1,12 @@
-public class Loops {
 
-   public static void main(String[] args) {
-    System.out.println("fixed it");
-   }
+public class Loops {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
 
     }
-    //hi boy
+}   
     
 
