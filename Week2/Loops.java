7f53a4afb416de99fd3f72ee095f0335d410747c
Farhan Raza <farhanwork08@gmail.com>
2026-08-11 05:58:56 +0530

    Update Loops.java

--- Week2/Loops.java
+++ Week2/Loops.java
@@ -2,11 +2,49 @@
 public class Loops {
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println(i);
-        }
+//        int i = 1;
+
+// while (i<=5) {
+
+//     System.out.println("While " +i);
+
+//     int j = 1;
+
+//     while (j<3) {
+
+//     System.out.println("Loop inside loop");
+//     j++;
+
+//     i++;
+
+        
+//     }
+
+// int x = 1;
+    
+
+//     do {
+//         System.out.println(x);
+//         x++;
+        
+//     } while (x>4);
+
+    
+// }
+
+int k;
+  for( k=1;k<=6;k++){
+    System.out.println(k);
+  }
+
+
+
+
+
+}
+
 
     }
-}   
+ 
     
 
