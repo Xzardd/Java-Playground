2a84362e337417c80dec4f8d40b18a06499a9dea
Farhan Raza <farhanwork08@gmail.com>
2026-08-12 23:57:13 +0530

    Update Objects.java

--- Week2/Objects.java
+++ Week2/Objects.java
@@ -1,3 +1,13 @@
-public class Objects {
-    
+class Car {
+    String color = "Red";
 }
+
+
+public class Objects {
+    public static void main(String[] args) {
+        Car myCar = new Car();
+
+        System.out.println(myCar.color);
+    }
+}
+
