99b13c8fe4df1027915ad50cdc75d9eefbfde1a6
Farhan Raza <farhanwork08@gmail.com>
2026-08-13 23:57:39 +0530

    Update Objects.java

--- Week2/Objects.java
+++ Week2/Objects.java
@@ -1,23 +1,47 @@
 class Student {
+
     String name;
     int age;
-    String branch;
-    double cgpa;
+    int marks;
 
-    Student(String name, int age, String branch, double cgpa) {
+    // Constructor
+    Student(String name, int age, int marks) {
         this.name = name;
         this.age = age;
-        this.branch = branch;
-        this.cgpa = cgpa;
+        this.marks = marks;
+    }
+
+    // Method
+    void displayDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Marks: " + marks);
+    }
+
+    // Method that works with object data
+    void checkResult() {
+        if (marks >= 40) {
+            System.out.println(name + " has passed.");
+        } else {
+            System.out.println(name + " has failed.");
+        }
     }
 }
 
 public class Objects {
     public static void main(String[] args) {
-        Student student1 = new Student("Farhan", 23, "Computer Science", 7.8);
 
-        System.out.println(student1.name);
-        System.out.println(student1.branch);
-        System.out.println(student1.cgpa);
+        // Creating objects
+        Student s1 = new Student("Rahul", 20, 85);
+        Student s2 = new Student("Aman", 21, 35);
+
+        // Calling methods using objects
+        s1.displayDetails();
+        s1.checkResult();
+
+        System.out.println();
+
+        s2.displayDetails();
+        s2.checkResult();
     }
 }
