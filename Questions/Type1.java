6aedc35c0997ba1658a73db2dcc8e9cc07d6b94f
Xzardd <farhanwork08@gmail.com>
2026-08-29 02:12:19 +0530

    Added Separate Folder For Questions

---
+++ Questions/Type1.java
@@ -0,0 +1,55 @@
+package Questions;
+
+public class Type1 {
+
+    // static double a = 10;
+    // static double b = 7;
+    // static char op = '/';
+
+    //static int x =  -32;
+
+    public static void main(String[] args) {
+    
+        //Basic calculator
+    //     if(op == '+'){
+    //         System.out.println(a+b);
+
+    // }
+    // else if (op == '-') {
+    //     System.out.println(a-b);
+        
+    // } else if (op == '*') {
+    //     System.out.println(a*b);
+        
+    // } 
+    // else if (op == '/') {
+    //     System.out.println(a/b);
+        
+    // }
+    // else{
+    //     System.out.println("Invalid Opeartor");
+    // }
+        
+ 
+    //Positive Negative and zero
+
+    // if ( x> 0) {
+    //     System.out.println("Positive");
+        
+    // } else if (x<0) {
+    //     System.out.println("Negative");
+        
+    // } else {
+    //     System.out.println("Zero");
+        
+    // }
+
+
+    //Largest of three Numbers
+    
+
+    }
+
+    
+    
+}
