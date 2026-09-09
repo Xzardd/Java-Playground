582c3d9a31a92742843fd7da443a4812f232fcbf
Xzardd <farhanwork08@gmail.com>
2026-09-09 11:46:33 +0530

    More Questions Added

--- Questions/LoopPrac.java
+++ Questions/LoopPrac.java
@@ -10,6 +10,16 @@ public class LoopPrac {
     // int a = 9;
     // int sum = 0;
     //  int m = 5;
+    // int f = 5;
+    // int factorial = 1;
+    // int num = 678986;
+    // int count = 0;
+    // int rum = 12345;
+    // int reverse = 0;
+    // int pam = 123;
+    // int rev = 0;
+    // int original = pam;
+    
 
     //Basic Loops Statement
     //      for(int n=1;n<=i;n++){
@@ -19,6 +29,7 @@ public class LoopPrac {
     //Sum of the loop
     // for(int b=1; b<=a;b++){
     //     sum = sum + b;
+    //     System.out.println(sum); (Each Value Gets Printed)
     // }
     //         System.out.println("Total Sum: " +sum);
 
@@ -26,6 +37,52 @@ public class LoopPrac {
     //  for(int x=1; x<=10; x++){
     //     System.out.println(m + " x " + x + " = " + (m * x));
     //  }
+
+    //Factorial
+    // for(int k=1; k<=f; k++){
+    //     factorial = factorial * k;
+    //     System.out.println(factorial); (Each Value gets printed)
+    // }
+    // System.out.println("Factorial = "+factorial);
+ 
+
+    //Counting Digits
+    // while(num !=0){
+    //     num = num / 10;
+    //     count++;
+    // }
+
+    // System.out.println("Total Sum: " +num);
+
+    //Reverse A Number
+    // while(rum != 0){
+    //     int digit = rum % 10;
+    //     reverse = reverse * 10 + digit;
+    //     rum = rum / 10;
+    // }
+
+    // System.out.println(reverse);
+
+    //Checking if its a pallindrome or not
+    // while(pam !=0){
+    //     int dig = pam % 10;
+    //     rev = rev * 10 + dig;
+    //     pam = pam/10;
+    // }
+    // System.out.println(rev);
+
+    // if(original == pam){
+    //     System.out.println("Palindrome");
+    // }
+    // else{
+    //     System.out.println("Not A Palindrome");
+    // }
+        
+
+
+
+
+  
 
 
 
