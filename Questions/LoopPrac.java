3ddf19c2f6a690d25e395be85bcd02877af3d239
Xzardd <farhanwork08@gmail.com>
2026-09-09 11:22:33 +0530

    3 Basic Loops Questions Added

--- Questions/LoopPrac.java
+++ Questions/LoopPrac.java
@@ -2,7 +2,33 @@ package Questions;
 
 
 public class LoopPrac {
+
+    // static int i = 11;
+
     public static void main(String[] args) {
+
+    // int a = 9;
+    // int sum = 0;
+    //  int m = 5;
+
+    //Basic Loops Statement
+    //      for(int n=1;n<=i;n++){
+    //             System.out.println("Loops values: " + n);
+    //    }
+
+    //Sum of the loop
+    // for(int b=1; b<=a;b++){
+    //     sum = sum + b;
+    // }
+    //         System.out.println("Total Sum: " +sum);
+
+    //Multiplication table
+    //  for(int x=1; x<=10; x++){
+    //     System.out.println(m + " x " + x + " = " + (m * x));
+    //  }
+
+
+
 
        
     }
