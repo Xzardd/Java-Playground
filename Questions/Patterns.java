b95ca3a1f90ca7c2ffa54cf4f14a5c5d34a8a849
Xzardd <farhanwork08@gmail.com>
2026-09-11 17:29:27 +0530

    More Questions Addressed

--- Questions/Patterns.java
+++ Questions/Patterns.java
@@ -2,12 +2,25 @@ package Questions;
 
 public class Patterns {
 
-      public static void main(String[] args) {
+
+    public static void main(String[] args) {
+
         int n = 5;
 
+        System.out.println("Pattern 1:");
+
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\nPattern 2:");
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
             }
             System.out.println();
         }
