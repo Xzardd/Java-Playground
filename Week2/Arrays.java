40b9988054f9b6048be17731c84853082c6f597d
Xzardd <farhanwork08@gmail.com>
2026-09-12 15:54:59 +0530

    Done till 2d Array

--- Week2/Arrays.java
+++ Week2/Arrays.java
@@ -3,15 +3,37 @@
 public class Arrays {
     public static void main(String[] args) {
 
-        //Basic Syntax
-        int[] arr = new int[]{10, 20, 30, 40, 50};
+    //Basic Syntax
+    //     int[] arr = new int[]{10, 20, 30, 40, 50};
 
-        for(int i = 0; i<arr.length;i++){
-            System.out.println(arr[i]);
+    //     for(int i = 0; i<arr.length;i++){
+    //         System.out.println(arr[i]);
 
-        }
+    //     }
        //Printing Length Also
-        System.out.println(arr.length);
+    //     System.out.println(arr.length);
+
+    //2d Array
+    // int [][] marks = new int[3][3];
+
+    // marks[0][0] = 9;
+    // marks[0][1] = 8;
+    // marks[0][2] = 7;
+    // marks[1][0] = 6;
+    // marks[1][1] = 5;
+    // marks[1][2] = 4;
+    // marks[2][0] = 3;
+    // marks[2][1] = 2;
+    // marks[2][2] = 1;
+
+    // for(int row = 0; row<3; row++){
+    //     for(int col = 0; col<3;col++){
+    //     System.out.print(marks[row][col]);
+
+    //     }
+    //     System.out.println();        
+    // }
+
 
         
     }
