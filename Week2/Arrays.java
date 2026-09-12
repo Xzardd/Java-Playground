9908ffbc24c3c2d680dffba9a2a55402356520fb
Xzardd <farhanwork08@gmail.com>
2026-09-12 15:29:04 +0530

    New Week Started

--- Week2/Arrays.java
+++ Week2/Arrays.java
@@ -3,12 +3,15 @@
 public class Arrays {
     public static void main(String[] args) {
 
+        //Basic Syntax
         int[] arr = new int[]{10, 20, 30, 40, 50};
 
         for(int i = 0; i<arr.length;i++){
             System.out.println(arr[i]);
 
         }
+       //Printing Length Also
+        System.out.println(arr.length);
 
         
     }
