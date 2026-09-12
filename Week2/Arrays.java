41927dd3cac9689d50d872847b7356a8a66d17cb
Xzardd <farhanwork08@gmail.com>
2026-09-12 14:38:00 +0530

    Learnt Basic Array Functioning

--- Week2/Arrays.java
+++ Week2/Arrays.java
@@ -3,7 +3,12 @@
 public class Arrays {
     public static void main(String[] args) {
 
-        
+        int[] arr = new int[]{10, 20, 30, 40, 50};
+
+        for(int i = 0; i<arr.length;i++){
+            System.out.println(arr[i]);
+
+        }
 
         
     }
