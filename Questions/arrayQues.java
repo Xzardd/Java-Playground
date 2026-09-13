41c0b4b0967714d6ad834381a5db76060fecb34e
Xzardd <farhanwork08@gmail.com>
2026-09-13 22:24:36 +0530

    Done With Array

--- Questions/arrayQues.java
+++ Questions/arrayQues.java
@@ -1,5 +1,42 @@
 package Questions;
 
 public class arrayQues {
+
+
+public static void main(String[] args) {
+
+    //Basic array printing 
+        // int[] arr = new int[]{10,20,30,40};
+        // System.out.println(arr[0]);
+    //Printing all elements
+        // for(int i = 0; i<arr.length; i++){
+        //     System.out.println(arr[i]);
+        // }    
+    //2D array printing
+        // int[][] marks = new int[2][2];
+
+        // marks[0][0] = 10; 
+        // marks[0][1] = 20; 
+        // marks[1][0] = 30;
+        // marks[1][1] = 40; 
+
+        // for(int rows = 0; rows<marks.length; rows++){
+        //     for(int col = 0; col<marks[rows].length; col++){
+        //         System.out.print(marks[rows][col]+ " ");
+        //     }
+        //     System.out.println("");
+        // }
+ 
+
+        
+        
+
     
 }
+    
+
+
+    
+    
+}
+
