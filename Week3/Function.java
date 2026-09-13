51ef65a720fa55540916a7afc285f51bf1c1e9ab
Xzardd <farhanwork08@gmail.com>
2026-09-13 23:59:55 +0530

    Function Overloading

--- Week3/Function.java
+++ Week3/Function.java
@@ -25,6 +25,26 @@ static int multiply(int a, int b){
     return (a * b);
 }
 
+static int sum(int a, int b){
+    return (a+b);
+
+}
+
+static int sum(int a, int b, int c){
+    return (a+b+c);
+}
+
+static void meet(String name, int age){
+    System.out.println("hello "+name + "your age is " +age);
+}
+
+static void meet(int age, String name) {
+    System.out.println("Your age is "+age+ "Your are " + name);
+}
+
+//Functon Overloading (sam name diff parameter)
+
+
     public static void main(String[] args) {
 
         //calling each function
@@ -38,6 +58,16 @@ static int multiply(int a, int b){
         // System.out.println(getNum()); this can also be used
        
         System.out.println(multiply(3, 6));
+
+        int z = sum(2, 4);
+        System.out.println(z);
+
+        int r = sum(2, 4, 6);
+        System.out.println(r);
+
+        meet("Farhan", 23);
+
+        meet(23, "Farhan");
        
     }
     
