97e2fa70ae849280938617e3f088bba512be8abd
Xzardd <farhanwork08@gmail.com>
2026-09-16 15:56:45 +0530

    Practiced Some Ques

---
+++ PracticePPR.java
@@ -0,0 +1,64 @@
+public class PracticePPR {
+
+    public static void main(String[] args) {
+
+        // Ques 5
+        // int a = 10;
+        // int b = 40;
+        // int c = 30;
+
+        // if (a>b && a>c) {
+
+        //     System.out.println("A");
+            
+        // } else if (b>a && b>c) {
+        //     System.out.println("B");
+            
+        // } else { 
+        //     System.out.println("C");
+            
+        // }
+
+        //Ques6
+
+        // int y = 1900;
+
+        // if (y%400==0 || (y%4==0 && y%100 != 0)) {
+        //     System.out.println("Its a leap year");
+            
+        // } else {
+        //     System.out.println("Not a leap year");
+            
+        //}
+
+        //Ques 7
+
+        // int Cel = 5;
+
+        // System.out.println((Cel * 9/5) + 32);
+
+        //Ques 8 swapping
+
+        // int a = 10;
+        // int b = 20;
+        
+        // System.out.println("Before Swapping");
+        // System.out.println(a);
+        // System.out.println(b);
+
+        // int temp = a;
+        // a=b;
+        // b=temp;
+
+        // System.out.println("After Swapping");
+        // System.out.println(a);
+        // System.out.println(b);
+
+        
+
+
+
+        
+    }
+    
+}
--- Questions/Type2.java
+++ Questions/Type2.java
@@ -1,66 +1,27 @@
 package Questions;
 
 public class Type2 {
+ public static void main(String[] args) {
 
-    //Fibonacci
+    //Fibonacci Series
+    // int n = 10;
 
-   public static void main(String[] args) {
-        if (System.console() == null) {
-            System.out.println("No console available. Run this program from a terminal.");
-            return;
-        }
+    // int a = 0;
+    // int b = 1;
 
-        System.out.print("Enter N for Fibonacci Series: ");
-        int n = Integer.parseInt(System.console().readLine().trim());
+    // for(int i = 1; i<n; i++){
 
-        if (n <= 0) {
-            System.out.println("Please enter a positive integer for Fibonacci.");
-        } else {
-            long first = 0, second = 1;
-            System.out.print("First " + n + " Fibonacci numbers: ");
-            for (int i = 1; i <= n; i++) {
-                System.out.print(first + " ");
-                long next = first + second;
-                first = second;
-                second = next;
-            }
-            System.out.println();
-        }
+    //     System.out.print(a + " ");
+
+    //     int c = a + b;
+    //     a=b;
+    //     b=c;
+
+    // }
 
 
-        System.out.print("Enter a number to check Armstrong: ");
-        int num = Integer.parseInt(System.console().readLine().trim());
 
-        if (num < 0) {
-            System.out.println(num + " is not an Armstrong number.");
-        } else {
-            int temp = num;
-            int digits = (temp == 0) ? 1 : 0;
-            while (temp > 0) {
-                digits++;
-                temp /= 10;
-            }
 
-            long sum = 0;
-            temp = num;
-            while (temp > 0) {
-                int digit = temp % 10;
-
-                long power = 1;
-                for (int i = 0; i < digits; i++) {
-                    power *= digit;
-                }
-                sum += power;
-
-                temp /= 10;
-            }
-
-            if (sum == num) {
-                System.out.println(num + " is an Armstrong number.");
-            } else {
-                System.out.println(num + " is not an Armstrong number.");
-            }
-        }
+        
     }
-    
 }
