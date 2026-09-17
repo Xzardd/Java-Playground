414da19902cf0db6cf0c262e9e0a4ee6fd905eb8
Xzardd <farhanwork08@gmail.com>
2026-09-17 15:26:29 +0530

    Learnt Encapsulation

--- Week3/Encapsulation.java
+++ Week3/Encapsulation.java
@@ -2,7 +2,57 @@ package Week3;
 
 public class Encapsulation {
     public static void main(String[] args) {
+
+        BankAccount ba = new BankAccount();
+        ba.deposit(500);
+        ba.withdraw(200);
+
+        System.out.println(ba.getBalance());
+
+
         
+
     }
     
+}
+
+class BankAccount{
+
+    private double balance;
+
+    public void deposit(int amount){
+        balance += amount;
+
+    }
+
+    public  void withdraw(int amount){
+        balance -= amount;
+    }
+
+    public double getBalance(){
+        return balance;
+
+    }
+
+}
+
+class Student{
+    String name;
+    int Age;
+    int rollNum;
+
+Student(String name , int age , int rollNum){
+    this.name = name;
+    this.Age = age;
+    this.rollNum = rollNum;
+}
+
+public String getName(){
+    return name;
+}
+
+public void setName(String name){
+    this.name = name;
+}
+
 }
