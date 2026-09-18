409a64f04198c74f03bacbb5dff3ca9852853de1
Xzardd <farhanwork08@gmail.com>
2026-09-18 23:13:41 +0530

    Started Inheritance

--- Week3/Inheritance.java
+++ Week3/Inheritance.java
@@ -1,5 +1,25 @@
 package Week3;
 
-public class Inheritance {
-    
+
+  class Animal {
+    void eat() {
+        System.out.println("Animal is eating");
+    }
 }
+
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Dog is barking");
+    }
+}
+
+public class Inheritance {
+    public static void main(String[] args) {
+
+        Dog d = new Dog();
+
+        d.eat();   
+        d.bark();  
+    }
+}  
+
