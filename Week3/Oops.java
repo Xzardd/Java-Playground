555ce8ff325f00f602d8ef56404f96ec7256f45e
Xzardd <farhanwork08@gmail.com>
2026-09-20 03:24:41 +0530

    Abstraction

--- Week3/Oops.java
+++ Week3/Oops.java
@@ -1,42 +1,35 @@
 package Week3;
 
 public class Oops {
-    
+
     public static void main(String[] args) {
 
-        Student s1 = new Student("Farhan",23,102,"Patna");
+        StudentX s1 = new StudentX("Farhan", 23, 102, "Patna");
 
-      
         s1.markAttendance();
         s1.print();
-        
     }
 }
 
-class Student {
+class StudentX {
 
-String name;
-int age;
-int rolNum;
-String city;
+    String name;
+    int age;
+    int rolNum;
+    String city;
 
-Student(String name, int age, int rolNum, String city){
+    StudentX(String name, int age, int rolNum, String city) {
+        this.name = name;
+        this.age = age;
+        this.rolNum = rolNum;
+        this.city = city;
+    }
 
-    this.name = name;
-    this.age = age;
-    this.rolNum = rolNum;
-    this.city = city;
+    void markAttendance() {
+        System.out.println("This attendance is marked by " + name);
+    }
 
-}
-
-void markAttendance(){
-    System.out.println("This attendance is marked by " +name);
-
-}
-
-void print(){
-    System.out.println(name + " " + age + " " + rolNum + " " + city);
-
-}
-
+    void print() {
+        System.out.println(name + " " + age + " " + rolNum + " " + city);
+    }
 }
