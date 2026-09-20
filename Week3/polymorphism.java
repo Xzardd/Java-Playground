3bbb6fddf80358f073c3f674631db211f799b83c
Xzardd <farhanwork08@gmail.com>
2026-09-20 23:32:35 +0530

    Started Polymorphism

--- Week3/polymorphism.java
+++ Week3/polymorphism.java
@@ -1,5 +1,46 @@
 package Week3;
 
-public class polymorphism {
-    
+public class Polymorphism {
+    public static void main(String[] args) {
+
+        Animal a1 = new Dog();
+        Animal a2 = new Cat();
+
+        a1.sound();
+        a2.sound();
+    }}
+
+    class Animal {
+
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
 }
+
+class Dog extends Animal {
+
+    @Override
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+class Cat extends Animal {
+
+    @Override
+    void sound() {
+        System.out.println("Cat meows");
+    }
+}
+
+
+
+
+       
+    
+
+
+
+
+        
+
