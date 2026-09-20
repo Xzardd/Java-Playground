445c143261f32675dd9a1c774fe81ea7140900d6
Xzardd <farhanwork08@gmail.com>
2026-09-21 02:49:11 +0530

    Created Abstraction File

--- Week3/polymorphism.java
+++ Week3/polymorphism.java
@@ -1,50 +1,11 @@
 package Week3;
 
 
-class Payment {
 
-    void pay(double amount) {
-        System.out.println("Processing payment of ₹" + amount);
-    }
-}
-
-class UPI extends Payment {
-
-    @Override
-    void pay(double amount) {
-        System.out.println("Paid ₹" + amount + " using UPI");
-    }
-}
-
-class CreditCard extends Payment {
-
-    @Override
-    void pay(double amount) {
-        System.out.println("Paid ₹" + amount + " using Credit Card");
-    }
-}
-
-class Cash extends Payment {
-
-    @Override
-    void pay(double amount) {
-        System.out.println("Paid ₹" + amount + " using Cash");
-    }
-}
 
 public class Polymorphism {
 
     public static void main(String[] args) {
 
-        Payment payment;
-
-        payment = new UPI();
-        payment.pay(1500);
-
-        payment = new CreditCard();
-        payment.pay(2500);
-
-        payment = new Cash();
-        payment.pay(800);
     }
 }
