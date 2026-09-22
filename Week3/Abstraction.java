1937a28fa7958e1cae0de29adc2781104f58fa84
Xzardd <farhanwork08@gmail.com>
2026-09-23 03:32:28 +0530

    Learnt Abstraction More In Depth

--- Week3/Abstraction.java
+++ Week3/Abstraction.java
@@ -1,12 +1,65 @@
 package Week3;
 
 
+abstract class CarX {
+
+    void start(){
+        System.out.println("Car started");
+    }
+
+    abstract void accelerate();
+
+    abstract void  stopping();
+}
+
+class ElectricCar extends CarX{
+
+@Override 
+    void accelerate(){
+        System.out.println("E-car Moving");
+
+
+    }
+@Override 
+    void stopping(){
+        System.out.println("E-car Stopped");
+
+    }
+}
+
+class FuelCar extends CarX{
+
+@Override 
+    void accelerate(){
+        System.out.println("F-car Moving");
+
+
+    }
+@Override 
+    void stopping(){
+        System.out.println("F-car Stopped");
+
+    }
+}
+
 
 
 public class Abstraction {
     public static void main(String[] args) {
 
+        CarX carX = new FuelCar();
+        carX.start();
+        carX.accelerate();
+        carX.stopping();
 
-        
+        CarX carX2 = new ElectricCar();
+        carX2.start();
+        carX2.accelerate();
+        carX2.stopping();
+
+
+
+
+
     }
 }
